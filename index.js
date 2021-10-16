d873b54711f9d2ea422c31a9a8cb9cc6448a7805
Jordan <jordanlindgren@live.com>
2021-10-16 09:50:32 -1000

    added questions for JS

---
+++ index.js
@@ -0,0 +1,65 @@
+var Manager = require("./library/Manager");
+var Engineer = require("./library/Engineer");
+var Intern = require("./library/Intern");
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+function createChoices(){
+    inquirer.prompt([
+        (
+            type:"list",
+            name:"menu",
+            message:"welcome to the team", 
+            choices:["add manager", "add engineer", "add intern", "exit app"]
+
+        )
+    ]).then( ({menu}) => {
+        switch(menu){
+            case "add manager":
+                addManager()
+                break;
+            case "add engineer":
+                addEngineer()
+                break;
+            case "add intern":
+                addIntern()
+                break;
+            case "exit app":        
+                createPage()
+        }
+    })
+}
+function createPage(){
+
+}
+function addManager(){
+    inquirer.prompt([
+        {
+            type:"input", 
+            name:"employeename",
+            message:"enter employee name",
+            
+        },
+        {
+            type:"input", 
+            name:"id",
+            message:"enter employee id",
+            
+        },
+        {
+            type:"input", 
+            name:"employeeemail",
+            message:"enter employee email",
+            
+        },
+        {
+            type:"input", 
+            name:"officeNumber",
+            message:"enter employee office number",
+            
+        }
+    ]).then(({employeename,id,employeeemail,officeNumber}) => {
+        console.log("Man")
+    })
+}
+
